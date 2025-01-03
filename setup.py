6f739ae21fc11e171b2f858b47a8c0dadd24d312
brayanj4y <souopsylvain@gmail.com>
2025-01-03 14:59:38 -0700

    Updated dependencies on requiremnets.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mini-nlp-toolkit",
-    version="0.1",
+    name="MiniNLPToolkit",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "nltk",
         "spacy",
+        "pandas",
+        "python-docx",
+        "PyPDF2"
     ],
     description="A compact toolkit for basic NLP preprocessing tasks.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Souop Silvain Brayan",
     author_email="souopsylvain@gmail.com",
@@ -18,6 +21,14 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Linguistic"
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "mininlp=MiniNLPToolkit.main:run",  # Assuming there's a main entry point to the package
+        ]
+    },
+    include_package_data=True,
+    keywords="nlp preprocessing text tokenization stopwords stemming lemmatization",
 )
